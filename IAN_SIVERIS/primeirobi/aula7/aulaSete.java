084fbbff3ea526b8e5df5356c81217fceaf4fa84
Ian-Siveris <imlsiveris@minha.fag.edu.br>
2024-04-08 20:55:52 -0300

    salvar

---
+++ IAN_SIVERIS/primeirobi/aula7/aulaSete.java
@@ -0,0 +1,21 @@
+package primeirobi.aula7;
+
+public class aulaSete {
+    public static void main(String[] args) {
+
+        Ninja primeiroNinja = new Ninja();
+
+        primeiroNinja.nome = "Naruto";
+        primeiroNinja.idade = "16";
+        primeiroNinja.Aldeia = "Folha";
+        primeiroNinja.equipamento = "Shiriken";
+        primeiroNinja.cla = "Uzumaki";
+        primeiroNinja.nomeJutso = "Rasengan";
+
+        Ninja segundoNinja - new Ninja();
+
+        
+
+
+    }
+}
