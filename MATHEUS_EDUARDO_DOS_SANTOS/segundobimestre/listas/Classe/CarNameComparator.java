c9e10037bd8e8dbb7f33abdb19d9794ed2d25d1d
eimatheusinho <mateusediardosantos@gmail.com>
2024-05-27 21:07:50 -0300

    entregs lista

---
+++ MATHEUS_EDUARDO_DOS_SANTOS/segundobimestre/listas/Classe/CarNameComparator.java
@@ -0,0 +1,13 @@
+package segundobimestre.listas.Classe;
+
+
+import java.util.Comparator;
+
+public class CarNameComparator implements Comparator<Car> {
+
+    @Override
+    public int compare(Car firstCar, Car secondCar) {
+        return firstCar.getName().compareTo(secondCar.getName());
+    }
+    
+}
---
+++ MATHEUS_EDUARDO_DOS_SANTOS/segundobimestre/listas/Classe/Teacher.java
@@ -0,0 +1,25 @@
+package segundobimestre.listas.Classe;
+
+public class Teacher {
+    
+    private String name;
+
+    private String subject;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String materia) {
+        this.subject = materia;
+    }
+
+}
