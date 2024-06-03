684fa829a3eba0f1ab3c5486144aeabd41322967
JeffersonTeles <jeffersontelesdeoliveira@gmail.com>
2024-06-03 12:50:09 -0300

    adicionando lista 3

---
+++ JEFFERSON_TELES/segundobi/Aulas/aulaquatro/ComparableSample.java
@@ -0,0 +1,28 @@
+package segundobi.Aulas.aulaquatro;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class ComparableSample {
+
+    public static void main(String[] args) {
+
+        List<Car> cars = new ArrayList<>();
+
+        cars.add(new Car("Civic", "Honda", 2008));
+        cars.add(new Car("350z", "Nissan", 2006));
+        cars.add(new Car("GT-R", "Nissan", 2012));
+        cars.add(new Car("Supra", "Toyota", 2004));
+
+        System.out.println("Sem ordenação:");
+        cars.forEach(car -> System.out.println(car.getName() + " - " + car.getYear()));
+
+        // Ordenando
+        Collections.sort(cars);
+
+        System.out.println("\nOrdenado padrão da classe(ano):");
+        cars.forEach(car -> System.out.println(car.getName() + " - " + car.getYear()));
+    }
+
+}
