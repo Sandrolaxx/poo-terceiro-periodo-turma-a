b9c887bac6b86a50f615e31e86169857a7655e55
Pedro Guligurski <pedro.guligurski@alfama.lan>
2024-06-24 19:08:32 -0300

    Atualização Lista 4 pt2

--- PEDRO_HENRIQUE_GULIGURSKI/segundobimestre/listas/lista4/ConsultaBoleto.java
+++ PEDRO_HENRIQUE_GULIGURSKI/segundobimestre/listas/lista4/ConsultaBoleto.java
@@ -2,24 +2,58 @@ package listas.lista4;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 
 import javax.swing.JOptionPane;
 
 public class ConsultaBoleto {
     public static void main(String[] args) {
+        consultarBoleto();
+    }
+
+    public static void consultarBoleto() {
         try {
-            // Lê a linha digitável do usuário (simulação)
             String linhaDigitavel = JOptionPane.showInputDialog("Informe a linha digitável:");
 
             // URL da API de consulta de boletos
-            URL url = new URL("https://developers.celcoin.com.br/api/v1/consulta-boleto?linhaDigitavel=" + linhaDigitavel);
-
+            URL url = new URL("https://sandbox.openfinance.celcoin.dev/v5/transactions/billpayments/authorize");
             // Abre conexão HTTP
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestMethod("GET");
-            connection.setRequestProperty("Accept", "application/json");
+        
+
+            String json = "{\r\n" + //
+                                "    \"externalTerminal\": \"Terminal de identificação externa do sistema do cliente, Ex: CPF\",\r\n" + //
+                                "    \"externalNSU\": 0, //Identificador da transação do sistema cliente\r\n" + //
+                                "    \"barCode\": {\r\n" + //
+                                "        \"type\": 2,\r\n" + //
+                                "        \"digitable\": \""+linhaDigitavel+"\",\r\n" + //
+                                "        \"barCode\": \"\"\r\n" + //
+                                "    }";
+        
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("accept", "application/json");
+            connection.setRequestProperty("Content-Type", "application/json");
+            connection.setRequestProperty("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjbGllbnRfaWQiOiI0MWI0NGFiOWE1NjQ0MC50ZXN0ZS5jZWxjb2luYXBpLnY1IiwiaHR0cDovL3NjaGVtYXMueG1sc29hcC5vcmcvd3MvMjAwNS8wNS9pZGVudGl0eS9jbGFpbXMvbmFtZSI6InRlc3RlIiwidGVuYW50X3VzZXIiOiI3cXlwS25NelMrcTBRaGtRYm9pWXIwVm5YOE9paFMxN2JUZkJjcHY1cHpUc3ovT0JIWHJnbjFOYUp1UVJYUlhFZ0t2UzBnZzJzbG8yOHVlcUFRaG1kNDZJZ29Qd3JiRU9ocU9IODViMndEODF2Z1pwdVRwbk9ndjhaamFaTi9hM1JuKytwUXpzSkFNbkxoK1BrbmZlc0g2ZEdyZXB2RXVGUFpNTzFLazh5K0tYcG4zQkpHWjV6RE9EWm0rKzhNVDI1QVJ0NVBNZ3M5STY2Z3VKTStIWDdBbmwyUHdISHBadFMrMXRqdThwVmkzeFpqbzZCOW5mOVJxNFFtdjA2dEpIWXZkcEZEUExybWNjM242RUNYRHhkQmM1MHlhSVJoTHpvcFNEVzNPb1VuT0NGaHRaVFRIbEc1K1lEdDNUVURvam1uU3VGVUY1OUdpcmUxWENXMmhRS0dqN3hkS3V5OWRLeHZpT3NTYW9COThCZ1daVmV6Y0NBZXhjeG9Eelg5VzJJbHlBRVJBVm9QdXNyTEg3NDNkc1k5R2g2d0dzWHFHYVZ6c3daSGtZdmFGZTdVRUtjUEhGSWtkTVh0c21qRWNpNzlXUXBEMVhKVktQMld5akZHTEw4SDkycGhSNU1hMDVZOXdob0xqWUNkcktDU0svc2tpYWRFT0dtQnV5T2RRdnhONnhaNWZHNHVyWjF5ZHBJYmN5TFhGbXVtMzdtbVlaT0pmMGpCUU9kaDBCeHB3Y01taXRkaE0vL3o1RzNIWnliVVRxcnYwL2lkS0hlVWN3MnVOK3NRPT0iLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3VzZXJkYXRhIjoiN2YyZGE2ZDE0MDVlNGM0M2E3YTIiLCJleHAiOjE3MTkyNjkxODUsImlzcyI6IkNlbGNvaW5BUEkiLCJhdWQiOiJDZWxjb2luQVBJIn0.gJ-srz9m8WcU56_f6TalyhALAGeQCesW_IOCZ0WBCzs");
+            connection.setDoOutput(true);
+
+            try (OutputStream os = connection.getOutputStream()) {
+                byte[] input = json.getBytes(StandardCharsets.UTF_8);
+                os.write(input, 0, input.length);
+            }
+
+            // Lendo o retorno JSON da requisição e atribuindo em uma String
+            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            StringBuilder response = new StringBuilder();
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+                response.append(line);
+            }
+
+            reader.close();
 
             // Verifica o código de resposta
             if (connection.getResponseCode() != 200) {
@@ -27,22 +61,12 @@ public class ConsultaBoleto {
                         + connection.getResponseCode());
             }
 
-            // Lê a resposta da API
-            BufferedReader br = new BufferedReader(new InputStreamReader(
-                    (connection.getInputStream())));
-
-            // Constrói a resposta
-            StringBuilder response = new StringBuilder();
-            String output;
-            while ((output = br.readLine()) != null) {
-                response.append(output);
-            }
+            String respo = response.toString();
 
             // Fecha conexão
             connection.disconnect();
 
-            // Exibe a resposta em um JOptionPane
-            JOptionPane.showMessageDialog(null, "Detalhes do Boleto:\n\n" + response.toString(), "Consulta de Boleto", JOptionPane.INFORMATION_MESSAGE);
+            JOptionPane.showMessageDialog(null, "Detalhes do Boleto:\n\n" + respo , "Consulta de Boleto", JOptionPane.INFORMATION_MESSAGE);
 
         } catch (Exception e) {
             e.printStackTrace();
--- PEDRO_HENRIQUE_GULIGURSKI/segundobimestre/listas/lista4/Main.java
+++ PEDRO_HENRIQUE_GULIGURSKI/segundobimestre/listas/lista4/Main.java
@@ -1,6 +1,12 @@
 package listas.lista4;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+
 
 public class Main {
     public static void main(String[] args) {
@@ -16,12 +22,10 @@ public class Main {
 
             switch (escolha) {
                 case 0:
-                    // Listar Convênios
                     listarConvenios();
                     break;
                 case 1:
-                    // Consulta de Boleto
-                    consultarBoleto();
+                    ConsultaBoleto.consultarBoleto();
                     break;
                 case 2:
                     // Sair do programa
@@ -39,19 +43,78 @@ public class Main {
         int choice = JOptionPane.showOptionDialog(null, "Selecione o tipo de convênio:",
                 "Seleção de Convênio", JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE,
                 null, options, options[0]);
-
+        String respo = "";
         if (choice == 0) {
-            System.out.println(ListagemConvenio.getConvenio(0)); 
+            respo = ListagemConvenio.getConvenio(0);
+            System.out.println(convenioFormatado(respo)); 
         } else if (choice == 1) {
-            System.out.println(ListagemConvenio.getConvenio(1)); 
+            respo = ListagemConvenio.getConvenio(1);
+            System.out.println(convenioFormatado(respo));  
         } else if (choice == 2) {
-            System.out.println(ListagemConvenio.getConvenio(2)); 
+            respo = ListagemConvenio.getConvenio(2);
+            System.out.println(convenioFormatado(respo)); 
         } else {
             System.out.println("Operação cancelada pelo usuário.");
         }
     }
 
+
+    
     public static void consultarBoleto() {
         // finalizar
     }
+
+    public static String convenioFormatado(String response) {
+        String jsonString = response;
+
+        jsonString = jsonString.replaceAll("\\s+", "")
+                               .replaceAll(",]", "]");
+
+        StringBuilder message = new StringBuilder();
+        message.append("Lista de Convênios:\n\n");
+
+        Pattern pattern = Pattern.compile("\\{(.*?)\\}");
+        Matcher matcher = pattern.matcher(jsonString);
+
+        JTextArea textArea = new JTextArea(20, 50); 
+        textArea.setEditable(false);
+        textArea.setText(message.toString());
+
+        JScrollPane scrollPane = new JScrollPane(textArea);
+
+        while (matcher.find()) {
+            String convenantStr = matcher.group();
+
+            String[] fields = convenantStr.split(",");
+            String nome = "";
+            String tipo = "";
+            String estado = "";
+            String horario = "";
+
+            for (String field : fields) {
+                if (field.contains("Nomeconvenant")) {
+                    nome = field.split(":")[1].replaceAll("\"", "");
+                } else if (field.contains("Tipoconvenant")) {
+                    tipo = field.split(":")[1].replaceAll("\"", "");
+                } else if (field.contains("statesconvenant")) {
+                    estado = field.split(":")[1].replaceAll("\"", "");
+                } else if (field.contains("timeLimit")) {
+                    horario = field.split(":")[1].replaceAll("\"", "");
+                }
+            }
+
+            message.append("Nome: ").append(nome).append("\n");
+            message.append("Tipo: ").append(tipo).append("\n");
+            message.append("Estado: ").append(estado).append("\n");
+            message.append("Horário limite: ").append(horario).append("\n");
+            message.append("----------------------------------\n");
+        }
+
+        textArea.setText(message.toString());
+
+        // Exibindo a mensagem em um JOptionPane com JScrollPane
+        JOptionPane.showMessageDialog(null, scrollPane, "Convênios", JOptionPane.INFORMATION_MESSAGE);
+        return null;
+    }
 }
+
